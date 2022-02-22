dbe3b8cb985332c7848fc81dde6ad5f554fe9b9d
talentMan726 <57292964+talentMan726@users.noreply.github.com>
2022-02-22 11:48:58 +0900

    test

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,6 @@
 var AngelAndDemonGame = artifacts.require("AngelAndDemonGame.sol");
 var AngelAndDemonToken = artifacts.require("AngelAndDemonToken.sol");
+var GodsNLegends = artifacts.require("GodsNLegends.sol");
 
 module.exports = function(deployer) {
   // deployer.deploy(AngelAndDemonToken)
@@ -8,5 +9,6 @@ module.exports = function(deployer) {
   // })
 
   // deployer.deploy(AngelAndDemonGame, "0x79cEcD2c185B46d49b4A2b1302519742436126c4");
-  deployer.deploy(AngelAndDemonGame, "0x5290d93EAE20439B3b54E68893049Ef5358305fC");  
+  // deployer.deploy(AngelAndDemonGame, "0x5290d93EAE20439B3b54E68893049Ef5358305fC");
+  deployer.deploy(GodsNLegends, "0x97BDE2489441FBF4BFC42712784ab95C4dA210C1")
 };
